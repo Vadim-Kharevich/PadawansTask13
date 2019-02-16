56bb6c5df5e9eebca3edfc6d6a4eccb7b033f240
Vadim-Kharevich <xarevich99@gmail.com>
2019-02-16 22:18:12 +0300

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string Surname;
+        private int Age;
+        public Employee()
+        {
+            Surname = string.Empty;
+            Age = 0;
+        }
+
+        public Employee(string surname, int age)
+        {
+            Surname = surname;
+            Age = age;
+        }
+
+        private string GetAge()
+        {
+            return Age.ToString();
+        }
+
+        public void ChangeSurname(string surname)
+        {
+            Surname = surname;
+        }
+
+        public string GetInfo()
+        {
+            return "Surname: {Employee.Surname}, Age: {Employee.Age}";
+        }
+    }
+}
